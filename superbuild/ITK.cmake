92a573f8041ebdc6934128b02175f37f4dd9f85a
Olivier Commowick <olivier.commowick@inria.fr>
2015-10-14 13:57:21 +0200

    ITK and VTK superbuilds point to release branch

--- superbuild/ITK.cmake
+++ superbuild/ITK.cmake
@@ -9,7 +9,7 @@ set (cmake_args
 
 set (location "")
 if (NOT DEFINED ${proj}_SRC_DIR)
-  set(tag v4.8.1)
+  set(tag release)
   set(location GIT_REPOSITORY ${GITHUB_PREFIX}InsightSoftwareConsortium/ITK.git GIT_TAG ${tag})
 endif()
 
--- superbuild/VTK.cmake
+++ superbuild/VTK.cmake
@@ -8,7 +8,7 @@ set (cmake_args
 
 set (location "")
 if (NOT DEFINED ${proj}_SRC_DIR)
-  set(tag v6.3.0)
+  set(tag release)
   set(location GIT_REPOSITORY ${GITHUB_PREFIX}Kitware/VTK.git GIT_TAG ${tag})
 endif()
 
