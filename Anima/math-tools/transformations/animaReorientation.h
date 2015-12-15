e2ade15211fac7fd93e49931eb6d8ff1d5f7b842
René-paul Debroize <rene-paul.debroize@irisa.fr>
2015-12-15 12:52:10 +0100

    Fix reorient function in dti estimator.

--- Anima/math-tools/transformations/animaReorientation.h
+++ Anima/math-tools/transformations/animaReorientation.h
@@ -168,7 +168,7 @@ reorientImageAndGradient(typename itk::SmartPointer<ImageType> input,
 
     for(unsigned int g = 0; g < gradients.size(); g++)
     {
-        GradientType reorientedGrad(3,0);
+        GradientType reorientedGrad(0.0);
         for(unsigned int i = 0; i < 3; ++i)
         {
             for(unsigned int j = 0; j < 3; ++j)
