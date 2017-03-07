203407f0752937871f61dde007505b4fdfc5913e
Olivier Commowick <olivier.commowick@inria.fr>
2017-03-07 15:54:06 +0100

    Correct stick compartment vector format

--- Anima/diffusion/mcm/animaStickCompartment.cxx
+++ Anima/diffusion/mcm/animaStickCompartment.cxx
@@ -1,6 +1,7 @@
 #include <animaStickCompartment.h>
 
 #include <animaVectorOperations.h>
+#include <itkSymmetricEigenAnalysis.h>
 
 namespace anima
 {
@@ -172,21 +173,42 @@ void StickCompartment::SetCompartmentVector(ModelOutputVectorType &compartmentVe
     if (compartmentVector.GetSize() != this->GetCompartmentSize())
         itkExceptionMacro("The input vector size does not match the size of the compartment");
 
+    Matrix3DType tensor, eVecs;
+    vnl_diag_matrix <double> eVals(m_SpaceDimension);
+    itk::SymmetricEigenAnalysis <Matrix3DType,vnl_diag_matrix <double>,Matrix3DType> eigSys(m_SpaceDimension);
+
+    unsigned int pos = 0;
+    for (unsigned int i = 0;i < m_SpaceDimension;++i)
+    {
+        for (unsigned int j = 0;j <= i;++j)
+        {
+            tensor(i,j) = compartmentVector[pos];
+
+            if (i != j)
+                tensor(j,i) = compartmentVector[pos];
+
+            ++pos;
+        }
+    }
+
+    eigSys.ComputeEigenValuesAndVectors(tensor,eVals,eVecs);
+
     Vector3DType compartmentOrientation, sphDir;
 
     for (unsigned int i = 0;i < m_SpaceDimension;++i)
-        compartmentOrientation[i] = compartmentVector[i];
+        compartmentOrientation[i] = eVecs(2,i);
 
     anima::TransformCartesianToSphericalCoordinates(compartmentOrientation,sphDir);
 
     this->SetOrientationTheta(sphDir[0]);
     this->SetOrientationPhi(sphDir[1]);
-    this->SetAxialDiffusivity(compartmentVector[this->GetCompartmentSize()-1]);
+    this->SetAxialDiffusivity(eVals(2));
+    this->SetRadialDiffusivity1((eVals(1) + eVals(0)) / 2.0);
 }
 
 unsigned int StickCompartment::GetCompartmentSize()
 {
-    return 4;
+    return 6;
 }
 
 unsigned int StickCompartment::GetNumberOfParameters()
@@ -212,10 +234,19 @@ StickCompartment::ModelOutputVectorType &StickCompartment::GetCompartmentVector(
     Vector3DType compartmentOrientation(0.0);
     anima::TransformSphericalToCartesianCoordinates(this->GetOrientationTheta(),this->GetOrientationPhi(),1.0,compartmentOrientation);
 
+    unsigned int pos = 0;
     for (unsigned int i = 0;i < m_SpaceDimension;++i)
-        m_CompartmentVector[i] = compartmentOrientation[i];
+    {
+        for (unsigned int j = 0;j <= i;++j)
+        {
+            m_CompartmentVector[pos] = (this->GetAxialDiffusivity() - this->GetRadialDiffusivity1()) * compartmentOrientation[i] * compartmentOrientation[j];
 
-    m_CompartmentVector[this->GetCompartmentSize()-1] = this->GetAxialDiffusivity();
+            if (i == j)
+                m_CompartmentVector[pos] += this->GetRadialDiffusivity1();
+
+            ++pos;
+        }
+    }
 
     return m_CompartmentVector;
 }
