2c18e5fd0bac9f236dca5e44f4790c3e64ff91d0
Olivier Commowick <olivier.commowick@inria.fr>
2017-03-21 11:14:21 +0100

    Change tclap link

--- superbuild/TCLAP.cmake
+++ superbuild/TCLAP.cmake
@@ -2,7 +2,7 @@ set (proj TCLAP)
 
 set (location "")
 if (NOT DEFINED ${proj}_SRC_DIR)
-  set(location URL http://vorboss.dl.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz)
+  set(location URL https://netix.dl.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz)
 endif()
 
 ExternalProject_Add(${proj}
