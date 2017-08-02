051f375764cc6df08daa07edc1f7776a5e6c05b8
Aymeric Stamm <aymeric@stamm.fr>
2017-08-02 18:59:03 +0200

    Removed copyright. Added references for Kummer functions. All other noise generators are deprecated.

--- Anima/math-tools/special_functions/animaKummerFunctions.h
+++ Anima/math-tools/special_functions/animaKummerFunctions.h
@@ -1,12 +1,3 @@
-/*
- *  animaKummerFunction.h
- *
- *
- *  Created by Aymeric Stamm on 07/12/2017.
- *  Copyright 2017 CRL. All rights reserved.
- *
- */
-
 #pragma once
 
 #include "AnimaSpecialFunctionsExport.h"
@@ -18,6 +9,7 @@ ANIMASPECIALFUNCTIONS_EXPORT
 double PochHammer(const double &x,
                   const unsigned int n);
 
+//! According to Muller, K. E. (2001) ‘Computing the confluent hypergeometric function, M (a, b, x)’, Numerische Mathematik, pp. 179–196. Method 1, p.5
 ANIMASPECIALFUNCTIONS_EXPORT
 double
 KummerMethod1(const double &x,
@@ -26,6 +18,7 @@ KummerMethod1(const double &x,
               const unsigned int maxIter = 10000,
               const double tol = 1.0e-15);
 
+//! According to Muller, K. E. (2001) ‘Computing the confluent hypergeometric function, M (a, b, x)’, Numerische Mathematik, pp. 179–196. Method 2, p.6
 ANIMASPECIALFUNCTIONS_EXPORT
 double
 KummerMethod2(const double &x,
@@ -34,6 +27,7 @@ KummerMethod2(const double &x,
               const unsigned int maxIter = 10000,
               const double tol = 1.0e-15);
 
+//! According to Muller, K. E. (2001) ‘Computing the confluent hypergeometric function, M (a, b, x)’, Numerische Mathematik, pp. 179–196. Switch between Method 1 and 2 according to recommandation at the end of page 5. Covers most situations (at least all common situations encountered in MR image processing).
 ANIMASPECIALFUNCTIONS_EXPORT
 double
 KummerFunction(const double &x,
