d18c4f29d2f9444f5940d59a39a055a527565710
Aymeric Stamm <aymeric@stamm.fr>
2017-09-20 20:09:40 +0200

    Added ML estimation of concentration parameter.

--- Anima/math-tools/statistical_distributions/animaVMFDistribution.h
+++ Anima/math-tools/statistical_distributions/animaVMFDistribution.h
@@ -12,6 +12,10 @@ double ComputeVMFPdf(const VectorType &v, const VectorType &meanDirection, const
 template <class ScalarType, unsigned int Dimension>
 double VMFDistance(const itk::Point <ScalarType,Dimension> &muFirst, const double &kappaFirst,
                    const itk::Point <ScalarType,Dimension> &muSec, const double &kappaSec);
+
+//! Implementation of an approximation of the MLE of the concentration parameter of the von Mises Fisher distribution according to Mardia, Directional statistics, 1972.
+template <class ScalarType>
+double GetVonMisesConcentrationMLE(const ScalarType rbar);
     
 } // end of namespace
 
