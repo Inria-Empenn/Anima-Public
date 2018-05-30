13020f5acf067df74cd84d46e870bed0ccb1358e
Aymeric Stamm <aymeric@stamm.fr>
2018-05-31 00:44:46 +0200

    Renamed flip tensor filter.

---
+++ Anima/diffusion/dti_tools/flip_tensors/animaFlipTensorImageFilter.hxx
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <itkImageRegionConstIterator.h>
+#include <itkImageRegionIterator.h>
+
+namespace anima
+{
+
+template <unsigned int ImageDimension>
+void
+FlipTensorImageFilter<ImageDimension>
+::GenerateOutputInformation()
+{
+    // Override the method in itkImageSource, so we can set the vector length of
+    // the output itk::VectorImage
+    
+    this->Superclass::GenerateOutputInformation();
+    
+    OutputImageType *output = this->GetOutput();
+    output->SetVectorLength(m_NumberOfComponents);
+}
+
+template <unsigned int ImageDimension>
+void
+FlipTensorImageFilter<ImageDimension>
+::ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
+                       itk::ThreadIdType threadId)
+{
+    itk::ImageRegionConstIterator<InputImageType> inItr(this->GetInput(), outputRegionForThread);
+    itk::ImageRegionConstIterator<MaskImageType> maskItr(this->GetComputationMask(), outputRegionForThread);
+    itk::ImageRegionIterator<OutputImageType> outItr(this->GetOutput(), outputRegionForThread);
+    
+    OutputPixelType outTensor(6);
+    
+    while (!maskItr.IsAtEnd())
+    {
+        if (maskItr.Get() == 0)
+        {
+            outTensor.Fill(0.0);
+            outItr.Set(outTensor);
+            
+            ++inItr;
+            ++outItr;
+            ++maskItr;
+            continue;
+        }
+        
+        outTensor = inItr.Get();
+        
+        if (m_FlippedAxis == "X")
+        {
+            outTensor[1] *= -1.0;
+            outTensor[3] *= -1.0;
+        }
+        else if (m_FlippedAxis == "Y")
+        {
+            outTensor[1] *= -1.0;
+            outTensor[4] *= -1.0;
+        }
+        else if (m_FlippedAxis == "Z")
+        {
+            outTensor[3] *= -1.0;
+            outTensor[4] *= -1.0;
+        }
+        
+        outItr.Set(outTensor);
+
+        ++inItr;
+        ++outItr;
+        ++maskItr;
+    }
+}
+
+} // end of namespace anima
