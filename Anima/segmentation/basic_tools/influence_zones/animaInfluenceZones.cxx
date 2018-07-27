32d5be00147102234eceaac71e0a85297051836a
Olivier Commowick <olivier.commowick@inria.fr>
2018-07-27 10:57:04 +0200

    Faster versions of influence zones and Otsu thresholding

--- Anima/segmentation/basic_tools/influence_zones/animaInfluenceZones.cxx
+++ Anima/segmentation/basic_tools/influence_zones/animaInfluenceZones.cxx
@@ -8,6 +8,7 @@
 #include <itkGrayscaleDilateImageFilter.h>
 #include <itkBinaryBallStructuringElement.h>
 #include <itkImageRegionIterator.h>
+#include <itkExtractImageFilter.h>
 
 #include <animaReadWriteFunctions.h>
 
@@ -37,9 +38,66 @@ int main(int argc, char **argv)
 
     InputImageType::Pointer inputImage = anima::readImage <InputImageType> (inArg.getValue());
 
+    InputImageType::RegionType reqRegion = inputImage->GetLargestPossibleRegion();
+    itk::ImageRegionConstIteratorWithIndex <InputImageType> inItr(inputImage,reqRegion);
+    int xMin = reqRegion.GetSize()[0];
+    int xMax = -1;
+    int yMin = reqRegion.GetSize()[1];
+    int yMax = -1;
+    int zMin = reqRegion.GetSize()[2];
+    int zMax = -1;
+
+    InputImageType::IndexType currentIndex;
+    while (!inItr.IsAtEnd())
+    {
+        if (inItr.Get() != 0)
+        {
+            currentIndex = inItr.GetIndex();
+            if (xMax < currentIndex[0])
+                xMax = currentIndex[0];
+            if (yMax < currentIndex[1])
+                yMax = currentIndex[1];
+            if (zMax < currentIndex[2])
+                zMax = currentIndex[2];
+
+            if (xMin >= currentIndex[0])
+                xMin = currentIndex[0];
+            if (yMin >= currentIndex[1])
+                yMin = currentIndex[1];
+            if (zMin >= currentIndex[2])
+                zMin = currentIndex[2];
+        }
+
+        ++inItr;
+    }
+
+    xMin = std::max(0,xMin - 2);
+    yMin = std::max(0,yMin - 2);
+    zMin = std::max(0,zMin - 2);
+
+    xMax = std::min((int)(reqRegion.GetIndex()[0] + reqRegion.GetSize()[0] - 1),xMax + 2);
+    yMax = std::min((int)(reqRegion.GetIndex()[1] + reqRegion.GetSize()[1] - 1),yMax + 2);
+    zMax = std::min((int)(reqRegion.GetIndex()[2] + reqRegion.GetSize()[2] - 1),zMax + 2);
+
+    reqRegion.SetIndex(0,xMin);
+    reqRegion.SetIndex(1,yMin);
+    reqRegion.SetIndex(2,zMin);
+
+    reqRegion.SetSize(0,xMax - xMin + 1);
+    reqRegion.SetSize(1,yMax - yMin + 1);
+    reqRegion.SetSize(2,zMax - zMin + 1);
+
+    // extract filter before and expand filter at the end to gain time
+    typedef itk::ExtractImageFilter<InputImageType, InputImageType> ExtractFilterType;
+    typename ExtractFilterType::Pointer extractFilter = ExtractFilterType::New();
+    extractFilter->SetExtractionRegion(reqRegion);
+    extractFilter->SetDirectionCollapseToGuess();
+    extractFilter->SetInput(inputImage);
+    extractFilter->SetNumberOfThreads(numThreadsArg.getValue());
+
     typedef itk::SignedDanielssonDistanceMapImageFilter <InputImageType, DistanceImageType, OutputImageType> DistanceMapFilterType;
     DistanceMapFilterType::Pointer distanceMap = DistanceMapFilterType::New();
-    distanceMap->SetInput(inputImage);
+    distanceMap->SetInput(extractFilter->GetOutput());
     distanceMap->InsideIsPositiveOn();
     distanceMap->SetUseImageSpacing(true);
     distanceMap->SetNumberOfThreads(numThreadsArg.getValue());
@@ -77,21 +135,31 @@ int main(int argc, char **argv)
     voronoiFilter->Update();
 
     OutputImageType::Pointer voronoiImage = voronoiFilter->GetVoronoiMap();
-    voronoiImage->DisconnectPipeline();
 
-    itk::ImageRegionIterator <OutputImageType> voronoiIt(voronoiImage,inputImage->GetLargestPossibleRegion());
-    itk::ImageRegionIterator <InputImageType> inputIt(inputImage,inputImage->GetLargestPossibleRegion());
+    OutputImageType::Pointer outImage = OutputImageType::New();
+    outImage->Initialize();
+    outImage->SetRegions(inputImage->GetLargestPossibleRegion());
+    outImage->SetSpacing (inputImage->GetSpacing());
+    outImage->SetOrigin (inputImage->GetOrigin());
+    outImage->SetDirection (inputImage->GetDirection());
+    outImage->Allocate();
+    outImage->FillBuffer(0);
+
+    itk::ImageRegionIterator <OutputImageType> voronoiIt(voronoiImage,voronoiImage->GetLargestPossibleRegion());
+    itk::ImageRegionIterator <OutputImageType> outIt(outImage,reqRegion);
+    itk::ImageRegionIterator <InputImageType> inputIt(inputImage,reqRegion);
 
     while (!inputIt.IsAtEnd())
     {
-        if (inputIt.Get() == 0)
-            voronoiIt.Set(0);
+        if (inputIt.Get() != 0)
+            outIt.Set(voronoiIt.Get());
 
         ++inputIt;
+        ++outIt;
         ++voronoiIt;
     }
 
-    anima::writeImage <OutputImageType> (outArg.getValue(),voronoiImage);
+    anima::writeImage <OutputImageType> (outArg.getValue(),outImage);
 
     return EXIT_SUCCESS;
 }
--- Anima/segmentation/basic_tools/otsu_thr_image/animaOtsuThrImage.cxx
+++ Anima/segmentation/basic_tools/otsu_thr_image/animaOtsuThrImage.cxx
@@ -1,6 +1,7 @@
 #include <tclap/CmdLine.h>
 
 #include <itkOtsuMultipleThresholdsImageFilter.h>
+#include <itkExtractImageFilter.h>
 
 #include <animaReadWriteFunctions.h>
 
@@ -11,8 +12,9 @@ int main(int argc, char **argv)
 {
     TCLAP::CmdLine cmd("INRIA / IRISA - Visages Team", ' ',ANIMA_VERSION);
     
-    TCLAP::ValueArg<std::string> inputArg("i","inputimage","Input image",true,"","Input image",cmd);
-    TCLAP::ValueArg<std::string> outputArg("o","outputimage","Output image",true,"","Output image",cmd);
+    TCLAP::ValueArg<std::string> inputArg("i","input","Input image",true,"","Input image",cmd);
+    TCLAP::ValueArg<std::string> maskArg("m","mask","Mask image for faster computation in a bounding box",false,"","Mask image",cmd);
+    TCLAP::ValueArg<std::string> outputArg("o","output","Output image",true,"","Output image",cmd);
 
     TCLAP::ValueArg<unsigned long> nbThresholds("n", "nbThresholds", "Number of thresholds (default : 1)",false,1,"Number of thresholds",cmd);
 
@@ -33,8 +35,79 @@ int main(int argc, char **argv)
 
     DoubleImageType::Pointer inputImage = anima::readImage <DoubleImageType> (inputArg.getValue());
 
+    USImageType::Pointer maskImage;
+    if (maskArg.getValue() != "")
+        maskImage = anima::readImage <USImageType> (maskArg.getValue());
+    else
+    {
+        maskImage = USImageType::New();
+        maskImage->Initialize();
+        maskImage->SetRegions(inputImage->GetLargestPossibleRegion());
+        maskImage->SetSpacing (inputImage->GetSpacing());
+        maskImage->SetOrigin (inputImage->GetOrigin());
+        maskImage->SetDirection (inputImage->GetDirection());
+        maskImage->Allocate();
+        maskImage->FillBuffer(1);
+    }
+
+    USImageType::RegionType reqRegion = inputImage->GetLargestPossibleRegion();
+    itk::ImageRegionConstIteratorWithIndex <USImageType> maskItr(maskImage,reqRegion);
+    int xMin = reqRegion.GetSize()[0];
+    int xMax = -1;
+    int yMin = reqRegion.GetSize()[1];
+    int yMax = -1;
+    int zMin = reqRegion.GetSize()[2];
+    int zMax = -1;
+
+    USImageType::IndexType currentIndex;
+    while (!maskItr.IsAtEnd())
+    {
+        if (maskItr.Get() != 0)
+        {
+            currentIndex = maskItr.GetIndex();
+            if (xMax < currentIndex[0])
+                xMax = currentIndex[0];
+            if (yMax < currentIndex[1])
+                yMax = currentIndex[1];
+            if (zMax < currentIndex[2])
+                zMax = currentIndex[2];
+
+            if (xMin >= currentIndex[0])
+                xMin = currentIndex[0];
+            if (yMin >= currentIndex[1])
+                yMin = currentIndex[1];
+            if (zMin >= currentIndex[2])
+                zMin = currentIndex[2];
+        }
+
+        ++maskItr;
+    }
+
+    xMin = std::max(0,xMin - 2);
+    yMin = std::max(0,yMin - 2);
+    zMin = std::max(0,zMin - 2);
+
+    xMax = std::min((int)(reqRegion.GetIndex()[0] + reqRegion.GetSize()[0] - 1),xMax + 2);
+    yMax = std::min((int)(reqRegion.GetIndex()[1] + reqRegion.GetSize()[1] - 1),yMax + 2);
+    zMax = std::min((int)(reqRegion.GetIndex()[2] + reqRegion.GetSize()[2] - 1),zMax + 2);
+
+    reqRegion.SetIndex(0,xMin);
+    reqRegion.SetIndex(1,yMin);
+    reqRegion.SetIndex(2,zMin);
+
+    reqRegion.SetSize(0,xMax - xMin + 1);
+    reqRegion.SetSize(1,yMax - yMin + 1);
+    reqRegion.SetSize(2,zMax - zMin + 1);
+
+    // extract filter before and expand filter at the end to gain time
+    typedef itk::ExtractImageFilter<DoubleImageType, DoubleImageType> ExtractFilterType;
+    typename ExtractFilterType::Pointer extractFilter = ExtractFilterType::New();
+    extractFilter->SetExtractionRegion(reqRegion);
+    extractFilter->SetDirectionCollapseToGuess();
+    extractFilter->SetInput(inputImage);
+
     OtsuMultipleThresholdsImageFilterType::Pointer otsuMultipleThrFilter = OtsuMultipleThresholdsImageFilterType::New();
-    otsuMultipleThrFilter->SetInput(inputImage);
+    otsuMultipleThrFilter->SetInput(extractFilter->GetOutput());
     otsuMultipleThrFilter->SetNumberOfThresholds(nbThresholds.getValue());
 
     try
@@ -47,7 +120,27 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    anima::writeImage <USImageType> (outputArg.getValue(),otsuMultipleThrFilter->GetOutput());
+    USImageType::Pointer outImage = USImageType::New();
+    outImage->Initialize();
+    outImage->SetRegions(inputImage->GetLargestPossibleRegion());
+    outImage->SetSpacing (inputImage->GetSpacing());
+    outImage->SetOrigin (inputImage->GetOrigin());
+    outImage->SetDirection (inputImage->GetDirection());
+    outImage->Allocate();
+    outImage->FillBuffer(0);
+
+    itk::ImageRegionIterator <USImageType> otsuIt(otsuMultipleThrFilter->GetOutput(),otsuMultipleThrFilter->GetOutput()->GetLargestPossibleRegion());
+    itk::ImageRegionIterator <USImageType> outIt(outImage,reqRegion);
+
+    while (!otsuIt.IsAtEnd())
+    {
+        outIt.Set(otsuIt.Get());
+
+        ++outIt;
+        ++otsuIt;
+    }
+
+    anima::writeImage <USImageType> (outputArg.getValue(),outImage);
     
     return EXIT_SUCCESS;
 }
