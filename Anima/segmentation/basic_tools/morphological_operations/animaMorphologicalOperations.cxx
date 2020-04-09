122d5ecd9552893aa02c5b477bbd72c667efca6a
Olivier Commowick <olivier.commowick@inria.fr>
2020-04-09 13:45:41 +0200

    Add option to use morpho operations with radius given in voxels

--- Anima/segmentation/basic_tools/morphological_operations/animaMorphologicalOperations.cxx
+++ Anima/segmentation/basic_tools/morphological_operations/animaMorphologicalOperations.cxx
@@ -15,7 +15,8 @@ int main(int argc, char **argv)
     TCLAP::ValueArg<unsigned int> nbpArg("p","nbcores","Number of cores to run on (default: all cores)",false,itk::MultiThreaderBase::GetGlobalDefaultNumberOfThreads(),"Number of cores",cmd);
 
     TCLAP::ValueArg<std::string> actArg("a","action","Action to perform ([clos], open, dil, er)",false,"clos","Action to perform",cmd);
-    TCLAP::ValueArg<double> radiusArg("r","radius","Radius of morphological operation in mm3",false,1,"morphological radius",cmd);
+    TCLAP::ValueArg<double> radiusArg("r","radius","Radius of morphological operation (in mm by default, see -R)",false,1,"morphological radius",cmd);
+    TCLAP::SwitchArg radiusVoxelArg("R","r-in-voxel","Use the radius in voxels",cmd);
 
     try
     {
@@ -40,27 +41,55 @@ int main(int argc, char **argv)
     ImageType::Pointer resPointer;
     BallElementType tmpBall;
 
-    // Compute Image Spacing
-    ImageType::SpacingType spacing = inputImage->GetSpacing();
-    ImageType::SpacingValueType spacing0 = spacing[0];
-    ImageType::SpacingValueType spacing1 = spacing[1];
-    ImageType::SpacingValueType spacing2 = spacing[2];
+    unsigned int radiusInVoxel0 = radiusArg.getValue();
+    unsigned int radiusInVoxel1 = radiusArg.getValue();
+    unsigned int radiusInVoxel2 = radiusArg.getValue();
 
-    double radiusInVoxelD0 = radiusArg.getValue() / spacing0;
-    double radiusInVoxelD1 = radiusArg.getValue() / spacing1;
-    double radiusInVoxelD2 = radiusArg.getValue() / spacing2;
+    if (!radiusVoxelArg.isSet())
+    {
+        // Compute Image Spacing
+        ImageType::SpacingType spacing = inputImage->GetSpacing();
+        ImageType::SpacingValueType spacing0 = spacing[0];
+        ImageType::SpacingValueType spacing1 = spacing[1];
+        ImageType::SpacingValueType spacing2 = spacing[2];
 
-    double radiusInVoxelD0_round = std::round(radiusInVoxelD0);
-    double radiusInVoxelD1_round = std::round(radiusInVoxelD1);
-    double radiusInVoxelD2_round = std::round(radiusInVoxelD2);
+        double radiusInVoxelD0 = radiusArg.getValue() / spacing0;
+        double radiusInVoxelD1 = radiusArg.getValue() / spacing1;
+        double radiusInVoxelD2 = radiusArg.getValue() / spacing2;
 
-    unsigned int radiusInVoxel0 = static_cast<unsigned int>(radiusInVoxelD0_round);
-    unsigned int radiusInVoxel1 = static_cast<unsigned int>(radiusInVoxelD1_round);
-    unsigned int radiusInVoxel2 = static_cast<unsigned int>(radiusInVoxelD2_round);
+        double radiusInVoxelD0_round = std::round(radiusInVoxelD0);
+        double radiusInVoxelD1_round = std::round(radiusInVoxelD1);
+        double radiusInVoxelD2_round = std::round(radiusInVoxelD2);
 
-    double diff0 = std::abs(radiusInVoxelD0-radiusInVoxelD0_round);
-    double diff1 = std::abs(radiusInVoxelD1-radiusInVoxelD1_round);
-    double diff2 = std::abs(radiusInVoxelD2-radiusInVoxelD2_round);
+        radiusInVoxel0 = static_cast<unsigned int>(radiusInVoxelD0_round);
+        radiusInVoxel1 = static_cast<unsigned int>(radiusInVoxelD1_round);
+        radiusInVoxel2 = static_cast<unsigned int>(radiusInVoxelD2_round);
+
+        std::cout << "Image spacing: " << spacing0 << "*" << spacing1 << "*" << spacing2  << std::endl;
+        std::cout << "Radius: " << radiusArg.getValue() << " mm3 --> " << "process on " << radiusInVoxel0 << "*" << radiusInVoxel1 << "*" << radiusInVoxel2 << " voxel(s)" << std::endl;
+
+        double diff0 = std::abs(radiusInVoxelD0-radiusInVoxelD0_round);
+        double diff1 = std::abs(radiusInVoxelD1-radiusInVoxelD1_round);
+        double diff2 = std::abs(radiusInVoxelD2-radiusInVoxelD2_round);
+
+        if (diff0 > 1.0e-6)
+        {
+            std::cout << "-- Warning: operation is not complete, on dimension 0 process is performed on " << (double)(radiusInVoxel0)*spacing0 << " mm3 ("  << radiusInVoxel0  << " voxel(s)) "
+                      << "instead of " << radiusArg.getValue() << " mm3 (" << radiusInVoxelD0 << " voxel(s))" << std::endl;
+        }
+
+        if (diff1 > 1.0e-6)
+        {
+            std::cout << "-- Warning: operation is not complete, on dimension 1 process is performed on " << (double)(radiusInVoxel1)*spacing1 << " mm3 ("  << radiusInVoxel1  << " voxel(s)) "
+                      << "instead of " << radiusArg.getValue() << " mm3 (" << radiusInVoxelD1 << " voxel(s))" << std::endl;
+        }
+
+        if (diff2 > 1.0e-6)
+        {
+            std::cout << "-- Warning: operation is not complete, on dimension 2 process is performed on " << (double)(radiusInVoxel2)*spacing2 << " mm3 ("  << radiusInVoxel2  << " voxel(s)) "
+                      << "instead of " << radiusArg.getValue() << " mm3 (" << radiusInVoxelD2 << " voxel(s))" << std::endl;
+        }
+    }
 
     BallElementType::SizeType ballSize;
     ballSize[0] = radiusInVoxel0;
@@ -71,7 +100,6 @@ int main(int argc, char **argv)
 
     if (actArg.getValue() == "er")
     {
-        std::cout << std::endl;
         std::cout << "Performing erosion with radius " << radiusArg.getValue() << "..." << std::endl;
 
         ErodeFilterType::Pointer mainFilter = ErodeFilterType::New();
@@ -85,7 +113,6 @@ int main(int argc, char **argv)
     }
     else if (actArg.getValue() == "dil")
     {
-        std::cout << std::endl;
         std::cout << "Performing dilation with radius " << radiusArg.getValue() << "..." << std::endl;
 
         DilateFilterType::Pointer mainFilter = DilateFilterType::New();
@@ -99,7 +126,6 @@ int main(int argc, char **argv)
     }
     else if (actArg.getValue() == "open")
     {
-        std::cout << std::endl;
         std::cout << "Performing opening with radius " << radiusArg.getValue() << "..." << std::endl;
 
         OpeningFilterType::Pointer mainFilter = OpeningFilterType::New();
@@ -113,7 +139,6 @@ int main(int argc, char **argv)
     }
     else
     {
-        std::cout << std::endl;
         std::cout << "Performing closing with radius " << radiusArg.getValue() << "..." << std::endl;
 
         ClosingFilterType::Pointer mainFilter = ClosingFilterType::New();
@@ -126,28 +151,6 @@ int main(int argc, char **argv)
         resPointer = mainFilter->GetOutput();
     }
 
-    std::cout << "Image spacing: " << spacing0 << "*" << spacing1 << "*" << spacing2  << std::endl;
-    std::cout << "Radius: " << radiusArg.getValue() << " mm3 --> " << "process on " << ballSize[0] << "*" << ballSize[1] << "*" << ballSize[2]  << " voxel(s)" << std::endl;
-
-    if (diff0 > 1.0e-6)
-    {
-        std::cout << "-- Warning: operation is not complete, on dimension 0 process is performed on " << (double)(radiusInVoxel0)*spacing0 << " mm3 ("  << radiusInVoxel0  << " voxel(s)) "
-                  << "instead of " << radiusArg.getValue() << " mm3 (" << radiusInVoxelD0 << " voxel(s))" << std::endl;
-    }
-
-    if (diff1 > 1.0e-6)
-    {
-        std::cout << "-- Warning: operation is not complete, on dimension 1 process is performed on " << (double)(radiusInVoxel1)*spacing1 << " mm3 ("  << radiusInVoxel1  << " voxel(s)) "
-                  << "instead of " << radiusArg.getValue() << " mm3 (" << radiusInVoxelD1 << " voxel(s))" << std::endl;
-    }
-
-    if (diff2 > 1.0e-6)
-    {
-        std::cout << "-- Warning: operation is not complete, on dimension 2 process is performed on " << (double)(radiusInVoxel2)*spacing2 << " mm3 ("  << radiusInVoxel2  << " voxel(s)) "
-                  << "instead of " << radiusArg.getValue() << " mm3 (" << radiusInVoxelD2 << " voxel(s))" << std::endl;
-    }
-
-    std::cout << std::endl;
     anima::writeImage <ImageType> (outArg.getValue(),resPointer);
 
     return EXIT_SUCCESS;
