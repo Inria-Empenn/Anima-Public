336341a49f77cf7ebd7127ceb08b7b1c0f926138
Olivier Commowick <olivier.commowick@inria.fr>
2020-04-20 15:00:15 +0200

    Update old school dwi simulator to handle all gradient formats

--- Anima/diffusion/dti_tools/dwi_simulator_from_dti/animaDWISimulatorFromDTI.cxx
+++ Anima/diffusion/dti_tools/dwi_simulator_from_dti/animaDWISimulatorFromDTI.cxx
@@ -1,86 +1,18 @@
 #include <animaDWISimulatorFromDTIImageFilter.h>
 
-#include <fstream>
-#include <itkImageFileReader.h>
-#include <itkImageFileWriter.h>
 #include <itkTimeProbe.h>
 
+#include <animaGradientFileReader.h>
+#include <animaReadWriteFunctions.h>
+
 #include <tclap/CmdLine.h>
-
-void loadGradients(std::string &gradsArg, std::vector< std::vector<float> >& directions)
-{
-    std::ifstream gradFile(gradsArg.c_str());
-    
-    if (!gradFile.is_open())
-    {
-        std::cerr << "Please provide file with gradient information" << std::endl;
-        abort();
-    }
-    
-    std::vector <float> gradTmp(3,0);
-    directions.clear();
-    
-    while (!gradFile.eof())
-    {
-        char tmpStr[2048];
-        gradFile.getline(tmpStr,2048);
-        
-        if (strcmp(tmpStr,"") == 0)
-            continue;
-        
-        sscanf(tmpStr,"%f %f %f",&gradTmp[0],&gradTmp[1],&gradTmp[2]);
-        
-        float normTmp = std::sqrt(gradTmp[0]*gradTmp[0] + gradTmp[1]*gradTmp[1] + gradTmp[2]*gradTmp[2]);
-        
-        if (normTmp > 0)
-        {
-            for (unsigned int i = 0;i < 3;++i)
-                gradTmp[i] /= normTmp;
-        }
-        
-        directions.push_back(gradTmp);
-    }
-    
-    gradFile.close();
-}
-
-void loadBValues(std::vector<std::vector<float> >& directions, std::string &bvalsArg, std::vector<float>& mb)
-{
-    mb.clear();
-    
-    std::ifstream bvalFile(bvalsArg.c_str());
-    
-    if (!bvalFile.is_open())
-    {        
-        for (unsigned int i = 0;i < directions.size();++i)
-            mb.push_back(std::stod(bvalsArg));
-        
-        return;
-    }
-    
-    float bvalTmp = 0;
-    
-    while (!bvalFile.eof())
-    {
-        char tmpStr[2048];
-        bvalFile.getline(tmpStr,2048);
-        
-        if (strcmp(tmpStr,"") == 0)
-            continue;
-        
-        sscanf(tmpStr,"%f",&bvalTmp);
-        mb.push_back(bvalTmp);
-    }
-    
-    bvalFile.close();
-}
 
 int main(int argc,  char **argv)
 {
     TCLAP::CmdLine cmd("INRIA / IRISA - VisAGeS/Empenn Team", ' ',ANIMA_VERSION);
-	
+
     TCLAP::ValueArg<std::string> inArg("i","inputdti","DTI volume",true,"","DTI volume",cmd);
-	TCLAP::ValueArg<std::string> resArg("o","output","Result DWI image",true,"","result DWI image",cmd);
+    TCLAP::ValueArg<std::string> resArg("o","output","Result DWI image",true,"","result DWI image",cmd);
     TCLAP::ValueArg<std::string> s0ValueArg("s","s0","S0 of DWI (constant value or image)",false,"200","S0 of DWI",cmd);
 
     TCLAP::ValueArg<std::string> gradsArg("g","grad","Input gradients",true,"","Input gradients",cmd);
@@ -101,68 +33,58 @@ int main(int argc,  char **argv)
     typedef anima::DWISimulatorFromDTIImageFilter<float> FilterType;
     typedef FilterType::InputImageType InputImageType;
     typedef FilterType::Image4DType Image4DType;
-
-    typedef itk::ImageFileReader <InputImageType> InputImageReaderType;
-    typedef itk::ImageFileWriter <Image4DType> Output4DImageWriterType;
     
     FilterType::Pointer mainFilter = FilterType::New();
     
-    std::vector<std::vector<float> > directions;
-    loadGradients(gradsArg.getValue(), directions);
+    typedef anima::GradientFileReader < std::vector <float>, float > GFReaderType;
+    GFReaderType gfReader;
+    gfReader.SetGradientFileName(gradsArg.getValue());
+    gfReader.SetBValueBaseString(bvalArg.getValue());
+    gfReader.SetGradientIndependentNormalization(false);
+    gfReader.Update();
+
+    std::vector< std::vector<float> > directions = gfReader.GetGradients();
     
     for(unsigned int i = 0;i < directions.size();++i)
         mainFilter->AddGradientDirection(i,directions[i]);
     
-    std::vector<float> mb;
-	loadBValues(directions, bvalArg.getValue(), mb);
-    
+    std::vector <float> mb = gfReader.GetBValues();
     mainFilter->SetBValuesList(mb);
-    
-    typedef itk::ImageFileReader <FilterType::S0ImageType> S0ImageReaderType;
-    S0ImageReaderType::Pointer s0TentativeReader = S0ImageReaderType::New();
-    s0TentativeReader->SetFileName(s0ValueArg.getValue());
 
+    typedef itk::ImageFileReader <FilterType::S0ImageType> S0ImageReaderType;
+    FilterType::S0ImageType::Pointer s0Image;
     try
     {
-        s0TentativeReader->Update();
-        mainFilter->SetS0Image(s0TentativeReader->GetOutput());
+        s0Image = anima::readImage <FilterType::S0ImageType> (s0ValueArg.getValue());
+        mainFilter->SetS0Image(s0Image);
     }
     catch(itk::ExceptionObject &e)
     {
         mainFilter->SetS0Value(std::stod(s0ValueArg.getValue()));
     }
 
-    InputImageReaderType::Pointer reader = InputImageReaderType::New();
-    reader->SetFileName(inArg.getValue());
-    
-    reader->Update();
-    mainFilter->SetInput(0,reader->GetOutput());
-
+    mainFilter->SetInput(anima::readImage <InputImageType> (inArg.getValue()));
     mainFilter->SetNumberOfWorkUnits(nbpArg.getValue());
     
     itk::TimeProbe tmpTimer;
-    
     tmpTimer.Start();
     
-    try {
+    try
+    {
         mainFilter->Update();
-    } catch (itk::ExceptionObject &e) {
+    }
+    catch (itk::ExceptionObject &e)
+    {
         std::cerr << e << std::endl;
         return EXIT_FAILURE;
     }
-        
+
     tmpTimer.Stop();
     
     std::cout << "Simulation done in " << tmpTimer.GetTotal() << " s" << std::endl;
-
     std::cout << "Writing result to : " << resArg.getValue() << std::endl;
 
-    Output4DImageWriterType::Pointer writer = Output4DImageWriterType::New();
-    writer->SetInput(mainFilter->GetOutputAs4DImage());
-    writer->SetFileName(resArg.getValue());
-    writer->SetUseCompression(true);
-    
-    writer->Update();
+    anima::writeImage <Image4DType> (resArg.getValue(), mainFilter->GetOutputAs4DImage());
 
     return EXIT_SUCCESS;
 }
