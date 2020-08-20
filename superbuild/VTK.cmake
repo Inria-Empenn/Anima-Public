159da92ab81bd67e67ca4e081bc9805472e4d729
Olivier Commowick <olivier.commowick@inria.fr>
2020-08-20 16:09:01 +0200

    Correct VTK superbuild

--- superbuild/VTK.cmake
+++ superbuild/VTK.cmake
@@ -9,7 +9,7 @@ set (cmake_args
 
 set (location "")
 if (NOT DEFINED ${proj}_SRC_DIR)
-  set(tag release)
+  set(tag origin/release)
   set(location GIT_REPOSITORY ${GITHUB_PREFIX}Kitware/VTK.git GIT_TAG ${tag})
 endif()
 
