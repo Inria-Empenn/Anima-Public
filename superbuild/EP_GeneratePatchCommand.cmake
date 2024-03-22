48591389646a398a21cebd42de2b1db5c53c6d56
FLORENT LERAY <florent.leray@inria.fr>
2024-03-22 12:24:38 +0100

    Import the patch system from medInria. Adapts it. Create a patch for ITK with the new GCC version.

---
+++ superbuild/EP_GeneratePatchCommand.cmake
@@ -0,0 +1,14 @@
+## #############################################################################
+## Generate patch command for an external project. To use with ExternalProject_Add
+## #############################################################################
+function(ep_GeneratePatchCommand ep OutVar patch)
+    set(PATCH_COMMAND ${CMAKE_COMMAND}
+                      -Dep:STRING=${ep}
+                      -DANIMA_SOURCE_DIR:STRING=${CMAKE_SOURCE_DIR}
+                      -DEP_PATH_SOURCE:STRING=${EP_PATH_SOURCE}
+                      -Dpatch:STRING=${patch} 
+                      -P
+                      ${CMAKE_SOURCE_DIR}/superbuild/EP_PatcherScript.cmake)
+
+    set(${OutVar} ${PATCH_COMMAND} PARENT_SCOPE)
+endfunction()
